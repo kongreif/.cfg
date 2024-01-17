7963143e6433b4b55b1bd7f1f586b341e6768ef4
kongreif <konstantin.greif@mailfence.com>
2024-01-17 15:36:06 +0100

    Modularize completion plugins

--- .config/nvim/lua/kongre/init.lua
+++ .config/nvim/lua/kongre/init.lua
@@ -21,13 +21,14 @@ require("lazy").setup({
     priority = 1000,
     opts = {},
   },
-  -- Snippets, dependency for other plugins
+  -- Plenary is a dependency for other plugins
   "nvim-lua/plenary.nvim",
   "BurntSushi/ripgrep",
   { "nvim-telescope/telescope.nvim", branch = "0.1.x" },
   "nvim-treesitter/nvim-treesitter",
   "nvim-treesitter/nvim-treesitter-textobjects",
   "nvim-treesitter/nvim-treesitter-context",
+  "nvim-tree/nvim-web-devicons",
   "mbbill/undotree",
   "theprimeagen/harpoon",
   {
@@ -48,72 +49,18 @@ require("lazy").setup({
    },
    -- TODO: Add keymaps and workflow for uses
   },
-  "nvim-tree/nvim-web-devicons",
   "nvim-lualine/lualine.nvim",
   { dir = "~/coding/code-palette.nvim" },
   -- completion
   {
     "hrsh7th/nvim-cmp",
     version = false, -- last release is way too old
-    event = "InsertEnter",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "saadparwaiz1/cmp_luasnip",
     },
-    opts = function()
-      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
-      local cmp = require("cmp")
-      local defaults = require("cmp.config.default")()
-      return {
-        completion = {
-          completeopt = "menu,menuone,noinsert",
-        },
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<S-CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<C-CR>"] = function(fallback)
-            cmp.abort()
-            fallback()
-          end,
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "path" },
-        }, {
-          { name = "buffer" },
-        }),
-        experimental = {
-          ghost_text = {
-            hl_group = "CmpGhostText",
-          },
-        },
-        sorting = defaults.sorting,
-      }
-    end,
-    ---@param opts cmp.ConfigSchema
-    config = function(_, opts)
-      for _, source in ipairs(opts.sources) do
-        source.group_index = source.group_index or 1
-      end
-      require("cmp").setup(opts)
-    end,
   },
   -- snippets
   {
