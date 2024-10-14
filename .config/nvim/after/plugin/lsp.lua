e89b4a6c03f405a1cda09d606b6f0087b4130dd9
Konstantin Greif <konstantin.greif@mailfence.com>
2024-10-14 13:44:09 +0200

    Add ruby-lsp

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -20,3 +20,34 @@ lspconfig.lua_ls.setup({
 		},
 	},
 })
+
+-- Attempt to use the project-specific 'ruby-lsp' if available
+local function get_ruby_lsp_command()
+	local is_bundled = vim.fn.glob("Gemfile") ~= ""
+	if is_bundled then
+		return "bundle exec ruby-lsp"
+	else
+		return "ruby-lsp"
+	end
+end
+
+lspconfig.ruby_lsp.setup({
+	cmd = { get_ruby_lsp_command(), "stdio" },
+	filetypes = { "ruby" },
+	root_dir = lspconfig.util.root_pattern(".git", "Gemfile"),
+	init_options = {
+		formatter = "rubocop",
+		linters = { "rubocop" },
+	},
+	settings = {
+		ruby = {
+			formatting = {
+				enabled = true,
+			},
+			linting = {
+				enabled = true,
+				linters = { "rubocop" },
+			},
+		},
+	},
+})
--- .config/nvim/after/plugin/markdown.lua
+++ .config/nvim/after/plugin/markdown.lua
@@ -7,3 +7,10 @@ require("mkdnflow").setup({
 
 -- Make sure that md files are saved, e.g. when navigating to different files
 vim.api.nvim_create_autocmd("FileType", { pattern = "markdown", command = "set awa" })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "markdown", "md" },
+	callback = function()
+		vim.keymap.set("i", "<CR>", "<Cmd>:MkdnNewListItem<CR>", { buffer = true, noremap = true, silent = true })
+	end,
+})
