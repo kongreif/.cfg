fa2d50ec7c74cc428cbc91928d49a81d16cd3a6e
Konstantin Greif <konstantin.greif@mailfence.com>
2025-07-26 17:42:45 +0200

    Extend terminal toggling and terminal rspec

--- .config/nvim/plugin/terminal.lua
+++ .config/nvim/plugin/terminal.lua
@@ -6,11 +6,11 @@ vim.api.nvim_create_autocmd("TermOpen", {
 	end,
 })
 
-local job_id = 0
+local channel_id = 0
 local buffer = -1
 local window = -1
 
-vim.keymap.set("n", "<leader>m", function()
+local toggle_terminal = function()
 	if vim.api.nvim_buf_is_valid(buffer) then
 		if vim.api.nvim_win_is_valid(window) then
 			if vim.api.nvim_get_current_win() == window then
@@ -31,11 +31,36 @@ vim.keymap.set("n", "<leader>m", function()
 		vim.api.nvim_win_set_height(0, 10)
 		buffer = vim.api.nvim_get_current_buf()
 		window = vim.api.nvim_get_current_win()
+		channel_id = vim.bo.channel
 	end
+end
 
-	job_id = vim.bo.channel
-end)
+local open_or_switch_to_terminal = function()
+	if vim.api.nvim_buf_is_valid(buffer) then
+		if vim.api.nvim_win_is_valid(window) then
+			if vim.api.nvim_get_current_win() ~= window then
+				vim.api.nvim_set_current_win(window)
+			end
+		else
+			window = vim.api.nvim_open_win(buffer, false, { split = "below" })
+			vim.api.nvim_set_current_win(window)
+			vim.cmd.wincmd("J")
+			vim.api.nvim_win_set_height(0, 10)
+		end
+	else
+		vim.cmd.new()
+		vim.cmd.term()
+		vim.cmd.wincmd("J")
+		vim.api.nvim_win_set_height(0, 10)
+		buffer = vim.api.nvim_get_current_buf()
+		window = vim.api.nvim_get_current_win()
+		channel_id = vim.bo.channel
+	end
+end
+
+vim.keymap.set("n", "<leader>m", toggle_terminal, { desc = "Terminal toggle" })
 
 vim.keymap.set("n", "<leader>rs", function()
-	vim.fn.chansend(job_id, { "echo 'hi'\r\n" })
-end)
+	open_or_switch_to_terminal()
+	vim.fn.chansend(channel_id, { "bundle exec rspec\r\n" })
+end, { desc = "Terminal rspec" })
