5898bfa76b92245ccfbbb2b5130e67a83d5eac26
Konstantin Greif <konstantin.greif@mailfence.com>
2025-07-26 16:09:20 +0200

    Add todo shortcuts

--- .config/nvim/after/plugin/editor_workflow.lua
+++ .config/nvim/after/plugin/editor_workflow.lua
@@ -5,7 +5,18 @@ require("gitblame").setup({
 })
 
 -- Keymaps for todo-comments.nvim
-vim.api.nvim_set_keymap("n", "<leader>tt", ":TodoTelescope<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>tt",
+	":TodoTelescope<CR>",
+	{ desc = "Todo telescope", noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>tq",
+	":TodoQuickFix<CR>",
+	{ desc = "Todo quickfix", noremap = true, silent = true }
+)
 
 -- Skip backwards compatibility routines and speed up loading of comment string
 vim.g.skip_ts_context_commentstring_module = true
