ebb08c77517cea45fc9d0f0bc0c04d585bb30ccd
kongreif <konstantin.greif@mailfence.com>
2026-03-17 15:55:59 +0100

    Fix treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,40 +1,43 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		branch = "master", -- Pin to old stable API (required for Neovim < 0.12)
 		lazy = false,
 		build = ":TSUpdate",
-		opts = {
-			ensure_installed = {
-				"bash",
-				"css",
-				"gitignore",
-				"go",
-				"json",
-				"jsonc",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"ruby",
-				"typescript",
-				"vue",
-			},
-			highlight = {
-				prefer_git = true,
-				disable_runtime_parsers = true,
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-		},
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"bash",
+					"css",
+					"gitignore",
+					"go",
+					"json",
+					"jsonc",
+					"lua",
+					"markdown",
+					"markdown_inline",
+					"ruby",
+					"typescript",
+					"vue",
+				},
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+			})
+		end,
 	},
 
 	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "master", -- Pin for compatibility with Neovim < 0.12
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
 		event = { "BufReadPost", "BufNewFile" },
 	},
 
 	{
 		"nvim-treesitter/nvim-treesitter-context",
+		branch = "master", -- Pin for compatibility with Neovim < 0.12
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
 		event = { "BufReadPost", "BufNewFile" },
 	},
